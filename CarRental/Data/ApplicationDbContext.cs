e163888f296f287a9d0000c244f1810e9293076e
Waleed Rehman <waleedrehmankhan@outlook.com>
2020-03-18 11:03:44 +1100

    initial Commit

---
+++ CarRental/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CarRental.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRental.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
