119ee6c70dff6ee2d8a97a94f914a8d7497d58c8
Waleed Rehman <waleedrehmankhan@outlook.com>
2020-03-27 11:49:34 +1100

    Seeding Membership Type

---
+++ CarRental/Migrations/20200327004009_PopulateMembershipType.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarRental.Migrations
+{
+    public partial class PopulateMembershipType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {   
+            migrationBuilder.Sql("INSERT INTO MembershipTypes (Id, Name, SignUpFee, DurationInMonths, DiscountRate) VALUES (1,'PayAsYouGo', 0, 0, 0)");
+            migrationBuilder.Sql("INSERT INTO MembershipTypes (Id, Name, SignUpFee, DurationInMonths, DiscountRate) VALUES (2,'Monthly', 30, 1, 10)");
+            migrationBuilder.Sql("INSERT INTO MembershipTypes (Id, Name, SignUpFee, DurationInMonths, DiscountRate) VALUES (3,'Quaterly', 90, 3, 15)");
+            migrationBuilder.Sql("INSERT INTO MembershipTypes (Id, Name, SignUpFee, DurationInMonths, DiscountRate) VALUES (4,'Yearly', 300, 12, 20)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
